c8ee3c6f536ac1834a5d48989c175a7114c045be
帝青的Mac <qinghuo@diqingdeMac.local>
2018-11-05 15:17:24 +0800

    Initial Commit

---
+++ FileShare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FileShare
+//
+//  Created by 帝青的Mac on 2018/11/5.
+//  Copyright © 2018年 帝青的Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FileShare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FileShare
+//
+//  Created by 帝青的Mac on 2018/11/5.
+//  Copyright © 2018年 帝青的Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
